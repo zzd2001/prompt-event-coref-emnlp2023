a2aba5901675713db38ddd802c1a5774ec3cecf4
zzd2001 <3102972860@qq.com>
2024-04-06 10:16:27 +0800

    注释arg.py

--- src/coref_prompt/arg.py
+++ src/coref_prompt/arg.py
@@ -19,9 +19,10 @@ def parse_args():
     parser.add_argument("--sample_strategy", default="corefnm", type=str, required=True, 
         help="The chosen sampling strategy.", choices=['no', 'random', 'corefnm', 'corefenn1', 'corefenn2']
     )
+    # 负采样参数
     parser.add_argument("--neg_top_k", default="3", type=int)
     parser.add_argument("--neg_threshold", default="0.2", type=float)
-    
+    #  模型参数
     parser.add_argument("--model_type", default="roberta", type=str, required=True, choices=['bert', 'roberta'])
     parser.add_argument("--model_checkpoint", default="roberta-large", type=str, required=True,
         help="Path to pretrained model or model identifier from huggingface.co/models.",
@@ -31,11 +32,15 @@ def parse_args():
     )
     parser.add_argument("--max_seq_length", default=512, type=int, required=True)
     parser.add_argument("--prompt_type", type=str, required=True)
+    # ？？ action="store_true"：这指定了当--with_mask被命令行指定时，应该对应的动作。store_true表示如果命令行中出现了--with_mask，那么相应的变量（这里是args.with_mask）将被设置为True。默认情况下，如果不使用--with_mask选项，args.with_mask将为False。
     parser.add_argument("--select_arg_strategy", type=str, required=True, choices=['no_filter', 'filter_related_args', 'filter_all'])
+    # ？？ 
     parser.add_argument("--with_mask", action="store_true", help="Whether to input the extra prompt with all triggers masked.")
+    # tensor matching的方式
     parser.add_argument("--matching_style", default="none", type=str, required=True, 
         help="Use tensor matching to help predict masked words.", choices=['no', 'product', 'cosine', 'product_cosine']
     )
+    
     parser.add_argument("--cosine_space_dim", type=int, help="Reduce event embedding dimension.")
     parser.add_argument("--cosine_slices", type=int, help="Cosine matching perspectives.")
     parser.add_argument("--cosine_factor", type=int, help="Tensor factorization.")
